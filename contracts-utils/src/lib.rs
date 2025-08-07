1b56de881fba80be39a9d113d53e02d4ad48c4d1
Nicolas Assouad <nicolasassouad75@gmail.com>
2025-08-07 15:25:17 +0200

    Continue to test redemption contract

--- contracts-utils/src/lib.rs
+++ contracts-utils/src/lib.rs
@@ -1,3 +1,6 @@
+#![no_std]
+#![allow(dead_code)]
+
 pub mod role {
     use soroban_sdk::{symbol_short, Symbol};
 
--- contracts/redemption/src/test.rs
+++ contracts/redemption/src/test.rs
@@ -3,21 +3,34 @@
 extern crate std;
 
 use super::contract::{Redemption, RedemptionArgs, RedemptionClient, RedemptionEntry};
+use contracts_utils::role::{WHITELISTED_ROLE, WHITELISTER_ROLE};
 use soroban_sdk::{
-    symbol_short,
+    log, symbol_short,
     testutils::{Address as _, Events},
     xdr::ToXdr,
-    Address, Env, Vec,
+    Address, Env, String, Vec,
 };
+
+mod permission_manager {
+    use soroban_sdk::contractimport;
+
+    contractimport!(file = "./permission_manager.wasm");
+}
+
+fn deploy_redemption(e: &Env) -> (Address, Address, RedemptionClient) {
+    let owner: Address = Address::generate(&e);
+    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
+    let client = RedemptionClient::new(&e, &contract_address);
+
+    (owner, contract_address, client)
+}
 
 #[test]
 fn test_should_set_owner_on_constructor() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
 
     let fetched_owner = client.get_owner();
 
@@ -29,9 +42,7 @@ fn test_set_permission_manager_should_require_owner_auth() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let permission_manager: Address = Address::generate(&e);
 
     client.set_permission_manager(&permission_manager);
@@ -47,9 +58,7 @@ fn test_add_token_should_require_owner_auth() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
 
     client.add_token(&token);
@@ -65,9 +74,7 @@ fn test_remove_token_should_require_owner_auth() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
 
     client.remove_token(&token);
@@ -83,9 +90,7 @@ fn test_on_redeem_should_fail_if_token_is_not_set() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let non_token_contract: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
 
@@ -99,9 +104,7 @@ fn test_on_redeem_should_pass_if_token_is_set() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
     client.add_token(&token);
@@ -116,9 +119,7 @@ fn test_on_redeem_should_require_token_auth() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
     client.add_token(&token);
@@ -136,9 +137,7 @@ fn test_on_redeem_should_fail_if_redemption_already_exists() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
     let salt: u128 = 100;
@@ -155,9 +154,7 @@ fn test_on_redeem_should_emit_a_redemption_initiated_event() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (_, contract_address, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
     let salt: u128 = 100;
@@ -191,15 +188,27 @@ fn test_execute_redemption_should_emit_a_redemption_executed_event() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let owner: Address = Address::generate(&e);
-    let contract_address = e.register(Redemption, RedemptionArgs::__constructor(&owner.clone()));
-    let client = RedemptionClient::new(&e, &contract_address);
+    let (owner, _, client) = deploy_redemption(&e);
     let token: Address = Address::generate(&e);
     let relayer: Address = Address::generate(&e);
     let user: Address = Address::generate(&e);
     let salt: u128 = 100;
     client.add_token(&token);
+    let permission_manager_id = e.register(
+        permission_manager::WASM,
+        permission_manager::Args::__constructor(&owner.clone()),
+    );
+    let permission_manager_client = permission_manager::Client::new(&e, &permission_manager_id);
 
+    let a = permission_manager_client
+        .get_admin()
+        .expect("Admin should be set");
+
+    assert_eq!(a, owner);
+
+    permission_manager_client.set_role_admin(&WHITELISTED_ROLE, &WHITELISTER_ROLE);
+
+    /*
     client.execute_redemption(&relayer, &token, &user, &100u128, &salt);
 
     let events = e.events().all();
@@ -221,4 +230,5 @@ fn test_execute_redemption_should_emit_a_redemption_executed_event() {
         event.2.to_xdr(&e),
         RedemptionEntry(token, user, 100, salt).to_xdr(&e)
     );
+    */
 }
