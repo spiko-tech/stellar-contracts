aaee8ebfc29e19e0ce3902c60f809ea5edf52254
Nicolas Assouad <nicolasassouad75@gmail.com>
2025-08-20 15:39:09 +0200

    Add revoke_role_batch

--- contracts/permission-manager/src/test.rs
+++ contracts/permission-manager/src/test.rs
@@ -131,13 +131,57 @@ fn test_grant_role_batch_whitelister_role_should_be_able_to_grant_whitelisted_ro
 }
 
 #[test]
-fn test_grant_role_batch_non_whitelister_role_should_not_be_able_to_grant_whitelisted_role_case_1()
-{
+fn test_grant_role_batch_non_whitelister_role_should_not_be_able_to_grant_whitelisted_role() {
     let e = setup_env();
     let (_admin, client) = deploy_permission_manager(&e);
     let non_whitelister: Address = Address::generate(&e);
     let whitelisted1: Address = Address::generate(&e);
     let whitelisted2: Address = Address::generate(&e);
+    let mut whitelisted = Vec::new(&e);
+    whitelisted.push_back(whitelisted1.clone());
+    whitelisted.push_back(whitelisted2.clone());
+
+    let result = client.try_grant_role_batch(&non_whitelister, &whitelisted, &WHITELISTED_ROLE);
+
+    let is_failure = match result {
+        Ok(Ok(_)) => false,
+        Ok(Err(_)) => true,
+        Err(_) => true,
+    };
+    assert!(is_failure, "Expected function to fail, but it succeeded");
+}
+
+#[test]
+fn test_revoke_role_batch_whitelister_role_should_be_able_to_revoke_whitelisted_roles() {
+    let e = setup_env();
+    let (admin, client) = deploy_permission_manager(&e);
+    let whitelister: Address = Address::generate(&e);
+    let whitelisted1: Address = Address::generate(&e);
+    let whitelisted2: Address = Address::generate(&e);
+    client.grant_role(&admin, &whitelister, &WHITELISTER_ROLE);
+    client.grant_role(&admin, &whitelisted1, &WHITELISTED_ROLE);
+    client.grant_role(&admin, &whitelisted2, &WHITELISTED_ROLE);
+    let mut whitelisted = Vec::new(&e);
+    whitelisted.push_back(whitelisted1.clone());
+    whitelisted.push_back(whitelisted2.clone());
+
+    client.revoke_role_batch(&whitelister, &whitelisted, &WHITELISTED_ROLE);
+
+    let has_role_whitelisted1: Option<u32> = client.has_role(&whitelisted1, &WHITELISTED_ROLE);
+    let has_role_whitelisted2: Option<u32> = client.has_role(&whitelisted2, &WHITELISTED_ROLE);
+    assert_eq!(has_role_whitelisted1, None);
+    assert_eq!(has_role_whitelisted2, None);
+}
+
+#[test]
+fn test_revoke_role_batch_non_whitelister_role_should_not_be_able_to_revoke_whitelisted_role() {
+    let e = setup_env();
+    let (admin, client) = deploy_permission_manager(&e);
+    let non_whitelister: Address = Address::generate(&e);
+    let whitelisted1: Address = Address::generate(&e);
+    let whitelisted2: Address = Address::generate(&e);
+    client.grant_role(&admin, &whitelisted1, &WHITELISTED_ROLE);
+    client.grant_role(&admin, &whitelisted2, &WHITELISTED_ROLE);
     let mut whitelisted = Vec::new(&e);
     whitelisted.push_back(whitelisted1.clone());
     whitelisted.push_back(whitelisted2.clone());
